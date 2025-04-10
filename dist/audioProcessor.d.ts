f174b5039a0f92bff0669f9f0163251fccc53e6a
Amir JCS <amirmahdyjdx@gmail.com>
2025-04-10 14:18:58 +0330

    build: npm package configs

---
+++ dist/audioProcessor.d.ts
@@ -0,0 +1,33 @@
+export declare class AudioProcessor extends EventTarget {
+    timeInterval: number;
+    freqStart: number;
+    freqEnd: number;
+    freqRange: number;
+    centerFrequency: number;
+    Q: number;
+    noiseMode: boolean;
+    alternating: boolean;
+    sumPSignal: number;
+    sumPNoise: number;
+    signalFrames: number;
+    noiseFrames: number;
+    rmsSignal: number;
+    rmsNoise: number;
+    snr: number;
+    audioContext: AudioContext | null;
+    mediaStream: MediaStream | null;
+    analyser: AnalyserNode | null;
+    bandPassFilter: BiquadFilterNode | null;
+    dataArray: Uint8Array | null;
+    fftSize: number;
+    updateTimerId: number | null;
+    rafId: number | null;
+    constructor(timeInterval?: number, freqStart?: number, freqEnd?: number, alternating?: boolean);
+    start(): Promise<void>;
+    stop(): void;
+    setIntervalTime(ms: number): void;
+    setAlternating(flag: boolean): void;
+    resetMeasurements(): void;
+    private _processAudio;
+    private _startUpdateTimer;
+}
